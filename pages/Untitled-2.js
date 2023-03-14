66cd92e8f7e7a163bad681b4e33904000f1ce7ff
Aakash <116252032+Aakashbalamurugan@users.noreply.github.com>
2023-03-14 16:16:01 +0530

    json

---
+++ pages/Untitled-2.js
@@ -0,0 +1,58 @@
+let a =[{
+    person_name:"Deepak Chopra",
+    images:{link:"../assets/img/session/Deepak_Chopra.jpg",alt:"Deepak Chopra"},
+    occupation:"Alternative medicine advocatepublic speakerwriter",
+    content:`is an Indian-American author and alternative medicine advocate. A prominent figure in
+    the New Age movement, his books and videos have made him one of the best-known and wealthiest figures in
+    alternative medicine.His discussions of quantum healing have been characterised as technobabble "incoherent
+    babbling strewn with scientific terms" which drives those who actually understand physics "crazy" and as
+    "redefining Wrong".`,
+    rating:4,
+    no_ratings:"(30 reviews)",
+
+},
+{
+    person_name:"Chris Gardner",
+    images:{link:"../assets/img/session/Chris_Gardner.jpg", alt:"Chris Gardner"},
+    occupation:"Businessman, motivational speaker ",
+    content:`is an American businessman and motivational speaker. During the early 1980s,
+    Gardner struggled with homelessness while raising a toddler son. He became a stockbroker and eventually
+    founded his own brokerage firm Gardner Rich & Co in 1987. In 2006, Gardner sold his minority stake in the
+    firm and published a memoir. That book was made into the motion picture The Pursuit of Happyness starring
+    Will Smith.`,
+    rating:3,
+    no_ratings:"(12 reviews)",
+
+},
+{
+    person_name:"Iyanla Vanzant",
+    images:{link:"../assets/img/session/Iyanla_Vanzant.png", alt:"Iyanla Vanzant"},
+    occupation:"Inspirational speaker, lawyer, spiritual teacher, author, TV personality",
+    content:`is an American inspirational speaker, lawyer, New Thought spiritual teacher, author,
+    life coach, and television personality. She is known primarily for her books, her eponymous talk show, and
+    her appearances on The Oprah Winfrey Show. She can currently be seen on television as the host of Iyanla:
+    Fix My Life, on OWN: Oprah Winfrey Network`,
+    rating:3,
+    no_ratings:"(12 reviews)",
+
+},
+{
+    person_name:"Darren Hardy",
+    images:{link:"../assets/img/session/Darren_Hardy.jpg",alt:"Darren Hardy"},
+    occupation:`Writer
+    CEO Advisor
+    High_Performance Trainer
+    Motivational Speaker `,
+    content:`is an American author, keynote speaker, advisor, and former publisher of SUCCESS
+    magazine. Hardy is a New York Times best-selling author, who wrote The Entrepreneur Roller Coaster, Living
+    Your Best Year Ever and The Compound Effect .`,
+    rating:3,
+    no_ratings:"(12 reviews)",
+
+}]
+
+
+
+
+
+
